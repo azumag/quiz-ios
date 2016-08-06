19820d9f4bf31d7dd05fc65ab09553daddbd98ec
Nuwaithu <nuwaithu@cloudsource.co.jp>
2016-08-06 15:44:53 +0630

    add show category

---
+++ Quiz/Quiz/CategoryTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  CategoryTableViewCell.swift
+//  Quiz
+//
+//  Created by Nu Wai Thu on 8/6/16.
+//  Copyright © 2016 Nu Wai Thu. All rights reserved.
+//
+
+import UIKit
+
+class CategoryTableViewCell: UITableViewCell {
+
+    
+    @IBOutlet weak var categoryname: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
